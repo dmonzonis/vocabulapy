beb51b1fa4530e6c1b1717e9af2b46f9603b5f68
monzo94 <monzo_94@hotmail.com>
2017-05-06 11:45:38 +0200

    Refactor dictionary utilities into DictionaryManager class

---
+++ dictionary_manager.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+VocabulaPy
+
+Small python game where you translate words.
+
+author: Daniel Monzonis
+website: github.com/monzo94
+"""
+
+import urllib.request
+import csv
+import json
+import os.path
+import pickle
+
+class DictionaryManager:
+    def __init__(self):
+        self.dictionary = {}
+
+    # Loads a list of words in English from CSV file and return it
+    def readWordList(self, filename):
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            wordList = list(reader)[0]
+        # Remove trailing space
+        for word in wordList:
+            wordList[wordList.index(word)] = word.strip()
+        return wordList
+
+    # Translates a word using internet API and return list of valid translations
+    def translate(self, word):
+        translationList = []
+        url = "https://glosbe.com/gapi/translate?from=eng&dest=es&format=json&phrase=%s" % word
+        response = urllib.request.urlopen(url).read()
+        data = json.loads(response)
+        if data['result'] != 'ok':
+            print("Error trying to translate word: %s" % word)
+            return None
+        for translation in data['tuc']:
+            if 'phrase' in translation:
+                translationList.append(translation['phrase']['text'])
+        return translationList
+
+    # Creates a dictionary of possible translations for given words (keys)
+    def createDict(self, wordList):
+        wordDict = dict()
+        listLength = len(wordList)
+        print("Translating %s words, please be patient" % str(listLength))
+        counter = 1
+        for word in wordList:
+            print("...%s/%s" % (str(counter), str(listLength)))
+            translations = self.translate(word)
+            wordDict[word] = translations
+            counter += 1
+        print("Done!")
+        return wordDict
+
+    # Creates a game-ready dictionary from a CSV file containing English words, using the Glosbe API
+    # to translate them if it wasn't previously generated before and saving it for future use, or loading
+    # the previously generated one if it exists
+    def load(self, filename):
+        if not os.path.exists(filename):
+            print("Could not find the word file!")
+            return
+        dictFilename = filename[:filename.find('.csv')] + '.dict'
+        if os.path.exists(dictFilename):
+            with open(dictFilename, 'rb') as f:
+                self.dictionary = pickle.load(f)
+                return
+        words = self.readWordList(filename)
+        wordDict = self.createDict(words)
+        with open(dictFilename, 'wb') as f:
+            pickle.dump(wordDict, f, protocol=pickle.HIGHEST_PROTOCOL)
+        self.dictionary = wordDict
--- game.py
+++ game.py
@@ -10,79 +10,20 @@ author: Daniel Monzonis
 website: github.com/monzo94
 """
 
-import urllib.request
-import csv
-import json
-import os.path
 import sys
-import pickle
 from random import choice
 from PyQt5.QtWidgets import QApplication, QMainWindow, qApp
 from PyQt5.QtCore import Qt, QEvent
+from dictionary_manager import DictionaryManager
 from gui import Ui_GameWindow
 
-# Loads a list of words in English from CSV file and return it
-def loadWordList(filename):
-    with open(filename, 'r') as f:
-        reader = csv.reader(f)
-        wordList = list(reader)[0]
-    # Remove trailing space
-    for word in wordList:
-        wordList[wordList.index(word)] = word.strip()
-    return wordList
-
-# Translates a word using internet API and return list of valid translations
-def translate(word):
-    translationList = []
-    url = "https://glosbe.com/gapi/translate?from=eng&dest=es&format=json&phrase=%s" % word
-    response = urllib.request.urlopen(url).read()
-    data = json.loads(response)
-    if data['result'] != 'ok':
-        print("Error trying to translate word: %s" % word)
-        return None
-    for translation in data['tuc']:
-        if 'phrase' in translation:
-            translationList.append(translation['phrase']['text'])
-    return translationList
-
-# Creates a dictionary of possible translations for given words (keys)
-def createDict(wordList):
-    wordDict = dict()
-    listLength = len(wordList)
-    print("Translating %s words, please be patient" % str(listLength))
-    counter = 1
-    for word in wordList:
-        print("...%s/%s" % (str(counter), str(listLength)))
-        translations = translate(word)
-        wordDict[word] = translations
-        counter += 1
-    print("Done!")
-    return wordDict
-
-# Creates a game-ready dictionary from a CSV file containing English words, using the Glosbe API
-# to translate them if it wasn't previously generated before and saving it for future use, or loading
-# the previously generated one if it exists
-def loadGameDict(filename):
-    if not os.path.exists(filename):
-        print("Could not find the word file!")
-        return
-    dictFilename = filename[:filename.find('.csv')] + '.dict'
-    if os.path.exists(dictFilename):
-        with open(dictFilename, 'rb') as f:
-            return pickle.load(f)
-    words = loadWordList(filename)
-    wordDict = createDict(words)
-    with open(dictFilename, 'wb') as f:
-        pickle.dump(wordDict, f, protocol=pickle.HIGHEST_PROTOCOL)
-    return wordDict
-
 class GameWindow(QMainWindow, Ui_GameWindow):
-    def __init__(self, window, dictionary):
+    def __init__(self, window, dictionaryManager):
         QMainWindow.__init__(self)
         Ui_GameWindow.__init__(self)
         self.setupUi(window)
         qApp.installEventFilter(self)
-        self.wordDict = dictionary
+        self.dManager = dictionaryManager
         self.word = ""
         self.keyPressed = False
 
@@ -101,14 +42,14 @@ class GameWindow(QMainWindow, Ui_GameWindow):
         return super(GameWindow, self).eventFilter(obj, event)
 
     def generateWord(self):
-        self.word = choice(list(wordDict.keys()))
+        self.word = choice(list(self.dManager.dictionary.keys()))
         self.wordText.setText(self.word.title())
 
     def processWord(self):
         text = self.lineEdit.text()
         self.lineEdit.setText('')
         self.lineEdit.setFocus()
-        if text in self.wordDict[self.word]:
+        if text in self.dManager.dictionary[self.word]:
             self.feedbackText.setText("CORRECT!")
         else:
             self.feedbackText.setText("You fail...")
@@ -116,11 +57,12 @@ class GameWindow(QMainWindow, Ui_GameWindow):
 
 
 if __name__ == '__main__':
-    wordDict = loadGameDict('qualities.csv')
+    dManager = DictionaryManager()
+    dManager.load('qualities.csv')
 
     app = QApplication(sys.argv)
     window = QMainWindow()
-    game = GameWindow(window, wordDict)
+    game = GameWindow(window, dManager)
 
     window.show()
     sys.exit(app.exec_())
