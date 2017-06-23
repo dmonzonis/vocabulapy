8c56a0313622c17dff0d24911227fd7274451c6b
monzo94 <monzo_94@hotmail.com>
2017-06-24 00:37:41 +0200

    Remove testing boilerplate code, now using pytest instead of unittest

--- tests/test_dictionary_manager.py
+++ tests/test_dictionary_manager.py
@@ -10,36 +10,30 @@ author: Daniel Monzonis
 website: github.com/monzo94
 """
 
-import unittest
-
 from vocabulapy.dictionary_manager import DictionaryManager
 
 
-class TestDictionaryManager(unittest.TestCase):
-
-    def setUp(self):
-        self.dictionaryManager = DictionaryManager()
-
-    def test_translate(self):
-        enToEsExamples = {
-            'olive': 'aceituna',
-            'accountant': 'contable',
-            'actress': 'actriz',
-            'designer': 'diseñador',
-            'engineer': 'ingeniero',
-            'confidence': 'confianza',
-            'disappointment': 'decepción',
-            'envy': 'envidia',
-            'luggage': 'equipaje',
-            'garbage': 'basura',
-            'clothing': 'ropa',
-            'anger': 'enfado',
-            'interview': 'entrevista',
-            'training': 'entrenamiento',
-            'dishwasher': 'friegaplatos'
-        }
-        enToEsExamples = {v: k for k, v in enToEsExamples.items()}
-        for k, v in enToEsExamples.items():
-            # Test English to Spanish and vice versa
-            self.assertIn(k, self.dictionaryManager.translate(v, 'en', 'es'))
-            self.assertIn(v, self.dictionaryManager.translate(k, 'es', 'en'))
+def test_translate():
+    dictionaryManager = DictionaryManager()
+    enToEsExamples = {
+        'olive': 'aceituna',
+        'accountant': 'contable',
+        'actress': 'actriz',
+        'designer': 'diseñador',
+        'engineer': 'ingeniero',
+        'confidence': 'confianza',
+        'disappointment': 'decepción',
+        'envy': 'envidia',
+        'luggage': 'equipaje',
+        'garbage': 'basura',
+        'clothing': 'ropa',
+        'anger': 'enfado',
+        'interview': 'entrevista',
+        'training': 'entrenamiento',
+        'dishwasher': 'friegaplatos',
+    }
+    enToEsExamples = {v: k for k, v in enToEsExamples.items()}
+    for k, v in enToEsExamples.items():
+        # Test English to Spanish and vice versa
+        assert(k in dictionaryManager.translate(v, 'en', 'es'))
+        assert(v in dictionaryManager.translate(k, 'es', 'en'))
